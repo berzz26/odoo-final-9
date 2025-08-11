17970a03c2e2341ebd2129137f420c8569d05ff2
berzz26 <aumtamboli2233@gmail.com>
2025-08-11 23:09:53 +0530

    feat: add route to get a specific trip

--- backend/src/routes/trip.routes.ts
+++ backend/src/routes/trip.routes.ts
@@ -5,7 +5,8 @@ import {
     createTrip,
     getTripById,
     updateTrip,
-    deleteTrip
+    deleteTrip,
+    getSpecificTrip
 } from "../controllers/trip.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 import { validate } from "../middlewares/zod.middleware.js";
@@ -16,8 +17,9 @@ const router = Router();
 // Routes for base trip management. All start with /
 router.get("/", authenticate, getAllTrips);
 router.post("/", authenticate, validate(tripSchema), createTrip);
-router.get("/:id", authenticate, getTripById);
+router.get("/", authenticate, getTripById);
 router.put("/:id", authenticate, validate(tripSchema), updateTrip);
 router.delete("/:id", authenticate, deleteTrip);
 
+router.get("/:id", authenticate, getSpecificTrip);
 export default router;
